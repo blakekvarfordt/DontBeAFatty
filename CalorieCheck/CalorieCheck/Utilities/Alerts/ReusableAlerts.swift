8a1394f2cb249c8eaedf45c92c02c84019825b83
Blake Kvarfordt <blakekvarfordt@gmail.com>
2020-01-11 12:53:04 -0700

    adds a cheating alert

--- CalorieCheck/CalorieCheck/Utilities/Alerts/ReusableAlerts.swift
+++ CalorieCheck/CalorieCheck/Utilities/Alerts/ReusableAlerts.swift
@@ -6,8 +6,25 @@
 //  Copyright © 2020 Blake kvarfordt. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class ResusableAlerts {
     
+    static func areYouCheatingAlert(vc: UIViewController) {
+        let alertController = UIAlertController(title: "Are you Cheating?", message: nil, preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let yes = UIAlertAction(title: "Yes", style: .default) { (_) in
+            // present apple purchase asking for a 1$ donation
+        }
+        
+        let no = UIAlertAction(title: "No", style: .default) { (_) in
+            // continue with the flow
+        }
+        
+        alertController.addAction(cancel)
+        alertController.addAction(yes)
+        alertController.addAction(no)
+        vc.present(alertController, animated: true)
+    }
 }
