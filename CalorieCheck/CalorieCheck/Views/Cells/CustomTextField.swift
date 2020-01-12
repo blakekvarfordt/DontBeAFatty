f7d8f3645d8ee24ce2f0b39000e75a5d3309dfac
Blake Kvarfordt <blakekvarfordt@gmail.com>
2020-01-11 17:55:36 -0700

    adds custom textfield

---
+++ CalorieCheck/CalorieCheck/Views/Cells/CustomTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTextField.swift
+//  CalorieCheck
+//
+//  Created by Blake kvarfordt on 1/11/20.
+//  Copyright © 2020 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .green {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+}
